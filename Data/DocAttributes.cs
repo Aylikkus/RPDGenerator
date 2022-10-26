38e2d07326e4d4b5beee13e2e7051419f5caf334
Alikku <4l1kku@gmail.com>
2022-10-26 17:47:10 +0300

    Create DocAttributes struct

---
+++ Data/DocAttributes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPDGenerator.Data
+{
+    struct DocAttributes
+    {
+        /// <summary>
+        /// Кафедра
+        /// </summary>
+        public string Departament { get; set; }
+    }
+}
