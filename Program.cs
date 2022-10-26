0e1f99168d775061408ef648ebcc8a5d4849df02
Alikku <4l1kku@gmail.com>
2022-10-26 16:36:49 +0300

    Update .NET Framework

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPDGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, world");
+            Console.ReadKey();
+        }
+    }
+}
