27f030d63cf7ecfd8389907702c7ff6bec3899f0
Alikku <4l1kku@gmail.com>
2022-10-28 19:34:22 +0300

    Pull departament

--- ExcelReader/ExcelReader.cs
+++ ExcelReader/ExcelReader.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Office.Interop.Excel;
+using RPDGenerator.Data;
 
 using Excel = Microsoft.Office.Interop.Excel;
 
@@ -6,5 +8,35 @@ namespace RPDGenerator.ExcelReader
 {
     static class ExcelReader
     {
+        /// <summary>
+        /// Вытягивает информацию о проф. дисциплине из эксель файла,
+        /// по определённому шаблону
+        /// </summary>
+        /// <param name="pathToFile">Путь к Эксель файлу/param>
+        /// <returns></returns>
+        public static DocAttributes PullAttributes(string pathToFile)
+        {
+            try
+            {
+                Application app = new Application();
+                Workbook workBook = app.Workbooks.Open(pathToFile,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing);
+
+                // Нумерация листов начинается с единицы
+                Worksheet title = workBook.Sheets[1];
+                string departament = ((Range)title.Cells[27, 3]).Value2;
+
+                DocAttributes da = new DocAttributes();
+                da.Departament = departament;
+                return da;
+            }
+            catch(Exception)
+            {
+                return new DocAttributes();
+            }
+        }
     }
 }
