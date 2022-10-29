b988e8a53cc004ca1bad4ca1c628e65108cef3d0
Alikku <4l1kku@gmail.com>
2022-10-29 20:21:48 +0300

    Test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using RPDGenerator.Data;
-using RPDGenerator.WordGenerator;
-using System;
+﻿using System;
+using RPDGenerator.Data;
+using RPDGenerator.Interops;
 
 namespace RPDGenerator
 {
@@ -8,11 +8,17 @@ namespace RPDGenerator
     {
         static void Main()
         {
-            string wordpattern = "C:\\Users\\HONOR\\Desktop\\ТЗ\\Макет.docx";
-            string excel = "C:\\Users\\HONOR\\Desktop\\ТЗ\\Excel\\2018\\очная\\10.05.04_ИАСБ_АИАД_УП(plx)_5.6_2018_~.xlsx";
-            DocAttributes dc = ExcelReader.ExcelReader.PullAttributes(excel);
-            WordGenerator.WordGenerator.WordHelper helper = new WordGenerator.WordGenerator.WordHelper(wordpattern);
-            helper.Process(dc);
+            string wordpattern = "C:\\Users\\4l1kk\\source\\repos\\RPDGenerator\\Макет.docx";
+            string excel = "C:\\Users\\4l1kk\\source\\repos\\RPDGenerator\\Excel\\2018\\очная\\10.05.04_ИАСБ_АИАД_УП(plx)_5.6_2018_~.xlsx";
+            DocAttributes dc;
+            using (ExcelReader er = new ExcelReader())
+            {
+                dc = er.PullAttributes(excel);
+            }
+            using (WordGenerator helper = new WordGenerator())
+            {
+                helper.Process(dc, wordpattern);
+            }
             Console.WriteLine("end");
             Console.ReadLine();
         }
