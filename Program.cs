3a3dbcd122cb817dcc984c216a9608a9fcda9a7b
Alikku <4l1kku@gmail.com>
2022-10-30 03:20:16 +0300

    optimize Word processing

--- Program.cs
+++ Program.cs
@@ -12,13 +12,9 @@ namespace RPDGenerator
             string excel = "C:\\Users\\4l1kk\\source\\repos\\RPDGenerator\\Excel\\2018\\очная\\10.05.04_ИАСБ_АИАД_УП(plx)_5.6_2018_~.xlsx";
             DocAttributes dc;
             using (ExcelReader er = new ExcelReader())
-            {
                 dc = er.PullAttributes(excel);
-            }
             using (WordGenerator helper = new WordGenerator())
-            {
-                helper.Process(dc, wordpattern);
-            }
+                helper.GenerateDocs(dc, wordpattern);
             Console.WriteLine("end");
             Console.ReadLine();
         }
