a1272981f5a8291b0d99e87a68664c27cd40dd4d
Alikku <4l1kku@gmail.com>
2022-10-31 16:53:16 +0300

    Add abbrevation to discipline

--- Data/Discipline.cs
+++ Data/Discipline.cs
@@ -1,14 +1,42 @@
 ﻿using System;
+using System.Text;
 
 namespace RPDGenerator.Data
 {
     public struct Discipline
     {
-        public string Code { get; set; }
-        public string Name { get; set; }
+        string _abbr;
+
+        void updateAbbrevation()
+        {
+            string[] words = Name.Split(new char[] { ' ', '-' },
+                        StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder abbr = new StringBuilder();
+            foreach (var w in words)
+            {
+                if (w.Length > 1)
+                    abbr.Append(char.ToUpper(w[0]));
+            }
+            _abbr = abbr.ToString();
+        }
+
+        public string Code { get; }
+        public string Name { get; }
+        public string Abbrevation
+        {
+            get
+            {
+                if (_abbr == null)
+                {
+                    updateAbbrevation();
+                }
+
+                return _abbr;
+            }
+        }
         // public int ClockCount { get; set; }
 
-        public Discipline(string code, string name)
+        public Discipline(string code, string name) : this()
         {
             Code = code;
             Name = name;
