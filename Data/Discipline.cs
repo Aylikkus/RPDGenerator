6079fa713947f2e9d8344ce8a12b724d0b43b9e3
Alikku <4l1kku@gmail.com>
2022-11-02 17:03:20 +0300

    Add bitFlags for semesters and courses

--- Data/Discipline.cs
+++ Data/Discipline.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace RPDGenerator.Data
@@ -6,6 +8,20 @@ namespace RPDGenerator.Data
     public struct Discipline
     {
         string _abbr;
+
+        /// <summary>
+        /// Представляет собой семестры, 
+        /// на которых идёт дисциплина
+        /// Например: 0b1001 -> на первом и четвёртом
+        /// </summary>
+        ushort _semesterFlag;
+
+        /// <summary>
+        /// Представляет собой курсы,
+        /// на которых идёт дисциплина
+        /// Например: 0b0101 -> на первом и третьем
+        /// </summary>
+        byte _courseFlag;
 
         void updateAbbrevation()
         {
@@ -42,6 +58,65 @@ namespace RPDGenerator.Data
             }
         }
         // public int ClockCount { get; set; }
+        private int[] arrayFromFlag(ushort flag)
+        {
+            ushort size = sizeof(ushort) * 8;
+            List<int> flagNums = new List<int>(size);
+
+            for (int i = size - 1; i >= 0; i--)
+            {
+                int bit = (flag >> i) & 1;
+                if (bit == 1)
+                    flagNums.Add(i + 1);
+            }
+
+            return flagNums.ToArray();
+        }
+
+        private int enableFlag(int flag, int number)
+        {
+            return flag | (1 << number - 1);
+        }
+
+        public int[] Semesters
+        {
+            get
+            {
+                return arrayFromFlag(_semesterFlag);
+            }
+        }
+
+        public int[] Courses
+        {
+            get
+            {
+                return arrayFromFlag(_courseFlag);
+            }
+        }
+
+        public void AddSemester(int number)
+        {
+            int size = sizeof(ushort);
+
+            if (number < 1 && number > size * 8)
+                return;
+
+            _semesterFlag = (ushort)enableFlag(_semesterFlag, number);
+            _courseFlag   = (byte)enableFlag(_courseFlag, number / 2 + number % 2);
+        }
+
+        public void AddCourse(int number)
+        {
+            int size = sizeof(byte);
+
+            if (number < 1 && number > size * 8)
+                return;
+
+            _courseFlag = (byte)enableFlag(_courseFlag, number);
+
+            _semesterFlag = (ushort)enableFlag(_semesterFlag, number * 2);
+            _semesterFlag = (ushort)enableFlag(_semesterFlag, number * 2 - number % 2);
+        }
 
         public Discipline(string code, string name) : this()
         {
