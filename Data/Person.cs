e912a5aa07554947f037ed3312d4126c29a6bb4b
Alikku <4l1kku@gmail.com>
2022-11-02 21:59:51 +0300

    Describe Persion class

---
+++ Data/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RPDGenerator.Data
+{
+    public class Person
+    {
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// Ученая степень
+        /// </summary>
+        public string AcademicDegree { get; set; }
+
+        /// <summary>
+        /// Ученое звание
+        /// </summary>
+        public string AcademicTitle { get; set; }
+
+        /// <summary>
+        /// Должность в вузе
+        /// </summary>
+        public string JobTitle { get; set; }
+    }
+}
