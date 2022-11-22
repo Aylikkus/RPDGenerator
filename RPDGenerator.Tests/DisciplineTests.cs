dc4f207893e74ec856ede46592f2825c75287e8d
Alikku <4l1kku@gmail.com>
2022-11-22 19:32:41 +0300

    add Discipline tests

---
+++ RPDGenerator.Tests/DisciplineTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPDGenerator.Data;
+using System;
+
+namespace RPDGenerator.Tests
+{
+    [TestClass]
+    public class DisciplineTests
+    {
+        [TestMethod]
+        public void Abbrevation_TLPRReturn()
+        {
+            string discName = "Технологии личностно-профессионального развития";
+            Discipline disc = new Discipline("example", discName);
+
+            string expected = "ТЛПР";
+
+            Assert.AreEqual(expected, disc.Abbrevation);
+        }
+
+        [TestMethod]
+        public void Abbrevation_EReturn()
+        {
+            string discName = "Экономика";
+            Discipline disc = new Discipline("example", discName);
+
+            string expected = "Э";
+
+            Assert.AreEqual(expected, disc.Abbrevation);
+        }
+
+        [TestMethod]
+        public void Abbrevation_ScopeReturn()
+        {
+            string discName = "История (история России, всеобщая история)";
+            Discipline disc = new Discipline("example", discName);
+
+            string expected = "И(ИРВИ)";
+
+            Assert.AreEqual(expected, disc.Abbrevation);
+        }
+    }
+}
